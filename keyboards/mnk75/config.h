68f486b10e7258b4ddb0a2251225b68d2367e76e
Hung Nguyen <hungnq3@vng.com.vn>
2022-11-15 09:50:39 +0700

    Modify mouse config

--- keyboards/mnk75/config.h
+++ keyboards/mnk75/config.h
@@ -67,3 +67,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 0
+
+/* Modify mouse config */
+#define MK_COMBINED
+#define MOUSEKEY_WHEEL_INTERVAL 20
+#define MOUSEKEY_WHEEL_MAX_SPEED 2
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 10
+#define MOUSEKEY_MAX_SPEED 2
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_TIME_TO_MAX 10
